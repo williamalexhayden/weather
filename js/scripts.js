1ff6e77550190a5c62d27c8227f5c06575a310d9
williamalexhayden <williamalexhayden@yahoo.com>
2015-03-19 03:32:48 -0700

    fade in quote placed

--- js/scripts.js
+++ js/scripts.js
@@ -131,45 +131,6 @@ var year = d.getFullYear();
     error: function(error) {
       $("#weather").html('<p>'+error+'</p>');
     }
-
-
   });//End Simple Weather
 
-
-
-// // 2. _simpleWeather()_ object
-//   $.simpleWeather({
-    
-//     location: '99004', // change zip
-//     unit: 'f',
-    
-//     // Get _weather_ object
-//     success: function(weather) {
-      
-//       // Get & store temperature
-//       var temp = weather.temp;
-//       // Get & store city
-//       var city = weather.city;
-//       //Get and store state
-//       var state = weather.region;
-//       console.log(state);
-//       var thumb = weather.thumbnail;
-//       console.log(thumb);
-//       // Output to hooks in HTML
-//       $('.temp').text(temp);
-//       $('.city').text(city);
-//       $('.state').text(state);
-//       $('.thumb img').attr('src', thumb);
-//       //attribute
-//       // See console for _weather_ object
-//       console.log(weather);
-//     },
-  
-//     // if error
-//     error: function(error) {  
-//       $('body').html('<p>' + error + '</p>');
-//     }
-//   });
-
-// end document.ready(function)
 });
