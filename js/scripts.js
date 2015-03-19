5a59ddde88ef31a6f140552093620252885c725a
williamalexhayden <williamalexhayden@yahoo.com>
2015-03-18 19:22:31 -0700

    on click functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-// REF: http://foundation.zurb.com/docs/
-// REF: http://simpleweatherjs.com/
+// REF: http://foundation.zurb.com/docs/    
+// REF: http://simpleweatherjs.com/   
 
 // begin document.ready(function)
  $(document).ready(function(){
@@ -37,39 +37,117 @@ var year = d.getFullYear();
   $('.time').text(time);
 
 
- // 2. _simpleWeather()_ object
+// input zip box
+  var zipCode = '99004';
+
+
+// on click weather gathered
+  $('.button').on('click', function(){
+
+    var zipCode = $('input').val();
+    console.log(zipCode);
+
     $.simpleWeather({
-      
-      location: '99004', // change zip
+      location: zipCode,
+      woeid: '',
       unit: 'f',
-      
-      // Get _weather_ object
+
       success: function(weather) {
+        icon = '<h2><i class="icon-'+weather.code+'"></i>';
+        html = '<p>'+weather.temp+'&deg;'+weather.units.temp+'</p>';
+        html = html + '<p>' + weather.currently + '<p>';
+        highlow = '<br/>' + 'high: ' +weather.forecast[0].high + ' low: ' +weather.forecast[0].low;
+
+        title = '<p>' + weather.city + ", " + weather.region + '<p>';
         
-        // Get & store temperature
-        var temp = weather.temp;
-        // Get & store city
-        var city = weather.city;
-        //Get and store state
-        var state = weather.region;
-        console.log(state);
-        var thumb = weather.thumbnail;
-        console.log(thumb);
-        // Output to hooks in HTML
-        $('.temp').text(temp);
-        $('.city').text(city);
-        $('.state').text(state);
-        $('.thumb img').attr('src', thumb);
-        //attribute
-        // See console for _weather_ object
-        console.log(weather);
+        tomorrowIcon = '<i class="icon-'+weather.forecast[1].code+'"></i>';
+        tomorrow = "Tomorrow's Weather: "+ weather.forecast[1].text + " " +tomorrowIcon;
+    
+      $(".weather").html(html);
+      $('.icon').html(icon);
+      $('.HighLow').html(highlow);
+      $('#location').html(title);
+      $('.tomorrow').html(tomorrow);
       },
-    
-      // if error
-      error: function(error) {  
-        $('body').html('<p>' + error + '</p>');
+      error: function(error) {
+        $("#weather").html('<p>'+error+'</p>');
       }
+    //END simpleWeather 
     });
-    
-// end document.ready(function)
+  //END on click weather gathered
   });
+
+
+  //WEATHER START
+  $.simpleWeather({
+      location: zipCode,
+      woeid: '',
+      unit: 'f',
+
+    success: function(weather) {
+      icon = '<h2><i class="icon-'+weather.code+'"></i>';
+      html = '<p>'+weather.temp+'&deg;'+weather.units.temp+'</p>';
+      html = html + '<p>' + weather.currently + '<p>';
+
+      title = '<p>' + weather.city + ", " + weather.region + '<p>';
+      highlow = '<br/>' + 'high: ' +weather.forecast[0].high + ' low: ' +weather.forecast[0].low;
+      console.log(weather.forecast[1].text);
+      
+      tomorrowIcon = '<i class="icon-'+weather.forecast[1].code+'"></i>';
+      tomorrow = "Tomorrow's Weather: "+ weather.forecast[1].text + " " +tomorrowIcon;
+
+      console.log(weather);
+
+      $(".weather").html(html);
+      $('.icon').html(icon);
+      $('.HighLow').html(highlow);
+      $('#location').html(title);
+      $('.tomorrow').html(tomorrow);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+
+  });//End Simple Weather
+
+
+  $('h1').text($.simpleWeather.location + "'s Weather")
+
+
+
+// // 2. _simpleWeather()_ object
+//   $.simpleWeather({
+    
+//     location: '99004', // change zip
+//     unit: 'f',
+    
+//     // Get _weather_ object
+//     success: function(weather) {
+      
+//       // Get & store temperature
+//       var temp = weather.temp;
+//       // Get & store city
+//       var city = weather.city;
+//       //Get and store state
+//       var state = weather.region;
+//       console.log(state);
+//       var thumb = weather.thumbnail;
+//       console.log(thumb);
+//       // Output to hooks in HTML
+//       $('.temp').text(temp);
+//       $('.city').text(city);
+//       $('.state').text(state);
+//       $('.thumb img').attr('src', thumb);
+//       //attribute
+//       // See console for _weather_ object
+//       console.log(weather);
+//     },
+  
+//     // if error
+//     error: function(error) {  
+//       $('body').html('<p>' + error + '</p>');
+//     }
+//   });
+
+// end document.ready(function)
+});
